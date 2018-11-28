abcd190030a7f3a0bab50c691bdf3932c45e4a7b
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2018-11-28 11:20:23 +0100

    LoaderProblem

--- src/mytunes/dal/TestDAO.java
+++ src/mytunes/dal/TestDAO.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
  */
 public class TestDAO
 {
-    public static void main(String[] args)
+    public static void main(String[] kokot)
     {
         try
         {
