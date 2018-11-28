458c4772b33769f7b3d38ceed6dd3ef47eaf9d46
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2018-11-28 12:57:47 +0100

    kookt

--- src/mytunes/dal/SongDAO.java
+++ src/mytunes/dal/SongDAO.java
@@ -8,7 +8,10 @@ package mytunes.dal;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,33 +32,88 @@ public class SongDAO
     
     public void removeSong(Song s)
     {
-        /*try(Connection con = conProvider.getConnection())
+        try(Connection con = conProvider.getConnection())
         {
-            String sql = "DELETE FROM Songs WHERE cprno =?";
+            String sql = "DELETE FROM Songs/Artists WHERE SongID =? ; DELETE FROM Playlists/Songs WHERE SongID = ?; DELETE FROM Songs WHERE ID = ?";
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, s.getId());
+            pstmt.setString(2, s.getId());
+            pstmt.setString(3, s.getId());
             pstmt.execute();
         } catch (SQLServerException ex)
         {
-            Logger.getLogger(PersonDAO.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex)
         {
-            Logger.getLogger(PersonDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     public void addSong(Song s)
     {
-        
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "INSERT INTO Songs(Title,Category,Duration,File) VALUES (?,?,?,?);";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, s.getTitle());
+            pstmt.setString(2, s.getCategory());
+            pstmt.setString(3, s.getDuration());
+            pstmt.setString(3, s.getFile());
+            pstmt.execute();
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public Song getSong(String name)
+    public Song getSong(int id)
     {
-        return null;
+        Song retval= null;
+        try(Connection con = conProvider.getConnection())
+        {
+            String sql = "SELECT Songs.*,Artists.Name FROM ((Songs INNER JOIN  WHERE ID = ? INNER JOIN Songs/Artists  ;";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, ""+id);
+            ResultSet rs = pstmt.executeQuery();
+            retval = new Song(rs.getInt("ID"),rs.getString("Title"),rs.getString("File"));
+            retval.setDuration(rs.getInt("Duration"));
+            retval.setCategory(rs.getString("Category"));
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retval;
     }
     
     public List<Song> getAllSongs()
     {
-       return null; 
+        List<Song> retval = new ArrayList<>();
+        try(Connection con = conProvider.getConnection())
+        {
+            String sqlStatement = "SELECT * FROM Songs";
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sqlStatement);
+            while(rs.next())
+            {
+                String id = rs.getString("ID");
+                String title = rs.getString("Title");
+                String job = rs.getString("Category");
+                Song s = new Song(id,title,job);
+                retval.add(s);
+            }
+        } catch (SQLServerException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(SongDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retval;
     }
 }
